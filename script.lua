493757e1fe1567c9890737b2262a36d57e910101
zenokosik <romarozdobudov@gmail.com>
2026-04-21 21:33:45 +0400

    script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,11 @@
-print("Hello from GitHub!")
+print("✅ Скрипт загружен успешно!")
+
+-- создаём Part в игре
+local part = Instance.new("Part")
+part.Size = Vector3.new(5, 1, 5)
+part.Position = Vector3.new(0, 10, 0)
+part.Anchored = true
+part.BrickColor = BrickColor.new("Bright green")
+part.Parent = workspace
+
+print("🟢 Парт создан!")
